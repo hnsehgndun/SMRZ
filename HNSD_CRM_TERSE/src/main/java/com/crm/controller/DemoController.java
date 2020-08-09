557a6d7b58ff2e7350a33217c701a13d06f080fb
JMH <13283838468>
2020-08-09 18:27:27 +0800

    测试

--- HNSD_CRM_TERSE/src/main/java/com/crm/controller/DemoController.java
+++ HNSD_CRM_TERSE/src/main/java/com/crm/controller/DemoController.java
@@ -1,4 +1,25 @@
 package com.crm.controller;
 
 public class DemoController {
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 0;
+        while (true){
+            a++;
+            if(b<10){
+                b = b + 3;
+                if(b>=10){
+                    break;
+                }else{
+                    b = b-2;
+                }
+            }else{
+                break;
+            }
+        }
+        System.out.println(a);
+
+    }
+
 }
