0ac2aaf9cccb10b5c253288878c007b5eeb90aac
24954 <2495404479@qq.com>
2020-08-10 17:23:51 +0800

    修改接口

--- HNSD_CRM_TERSE/src/main/java/com/crm/config/ApiFaceConfig.java
+++ HNSD_CRM_TERSE/src/main/java/com/crm/config/ApiFaceConfig.java
@@ -1,8 +1,6 @@
 package com.crm.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
--- HNSD_CRM_TERSE/src/main/java/com/crm/config/MyConfig.java
+++ HNSD_CRM_TERSE/src/main/java/com/crm/config/MyConfig.java
@@ -2,33 +2,37 @@ package com.crm.config;
 
 import com.crm.converter.DateConverter;
 import com.crm.interceptor.LoginHandlerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-@Component
+@Configuration
 public class MyConfig extends WebMvcConfigurationSupport {
-    /*@Override
-    protected void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/*")
-                //排除不需要拦截的请求路径
-                .excludePathPatterns("/login", "/", "/favicon.ico","/swagger*");
-                //springboot2+之后需要将静态资源文件的访问路径 也排除
-    }*/
+    @Bean
+    LoginHandlerInterceptor localInterceptor() {
+        return new LoginHandlerInterceptor();
+    }
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/login","user/register","/favicon.ico")
+                .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
-        //springboot 集成swagger2.2后静态资源404，添加如下两行配置
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
-        super.addResourceHandlers(registry);
     }
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new DateConverter());
