e9d5bcd40c9a4aa3af6017c32fa0dc0faf7c243b
woundplast <wgguozhilong@163.com>
2020-08-10 14:49:20 +0800

    修改  aipface 获取单例对象

--- HNSD_CRM_TERSE/src/main/java/com/crm/config/ApiFaceConfig.java
+++ HNSD_CRM_TERSE/src/main/java/com/crm/config/ApiFaceConfig.java
@@ -7,33 +7,36 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ApiFaceConfig {
-    @Value("#{configProperties['APP_ID']}")
-    private String APP_ID;
-    @Value("#{configProperties['API_KEY']}")
-    private String API_KEY;
-    @Value("#{configProperties['SECRET_KEY']}")
-    private String SECRET_KEY;
+//    @Value("#{configProperties['APP_ID']}")
+    private static String APP_ID;
+//    @Value("#{configProperties['API_KEY']}")
+    private static String API_KEY;
+//    @Value("#{configProperties['SECRET_KEY']}")
+    private static String SECRET_KEY;
 
-    public String getAPP_ID() {
+    public static String getAPP_ID() {
         return APP_ID;
     }
 
+    @Value("#{configProperties['APP_ID']}")
     public void setAPP_ID(String APP_ID) {
         this.APP_ID = APP_ID;
     }
 
-    public String getAPI_KEY() {
+    public static String getAPI_KEY() {
         return API_KEY;
     }
 
+    @Value("#{configProperties['API_KEY']}")
     public void setAPI_KEY(String API_KEY) {
         this.API_KEY = API_KEY;
     }
 
-    public String getSECRET_KEY() {
+    public static String getSECRET_KEY() {
         return SECRET_KEY;
     }
 
+    @Value("#{configProperties['SECRET_KEY']}")
     public void setSECRET_KEY(String SECRET_KEY) {
         this.SECRET_KEY = SECRET_KEY;
     }
